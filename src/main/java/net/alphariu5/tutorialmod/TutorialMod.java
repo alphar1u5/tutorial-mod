b592373a06d234c93ed45f229903a381a6d40c0a
Alpharius016 <h3kill.016@gmail.com>
2026-04-08 13:05:03 -0600

    Items Added

--- src/main/java/net/alphariu5/tutorialmod/TutorialMod.java
+++ src/main/java/net/alphariu5/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.alphariu5.tutorialmod;
 
+import net.alphariu5.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItems.registerModItems();
 
 	}
 }
